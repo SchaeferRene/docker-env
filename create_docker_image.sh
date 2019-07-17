19129cd3cfc2054e41f8ec55dc6cf75f33347bb3
R. Schaefer <31whte1tsb5t@opayq.com>
2019-07-18 01:52:58 +0200

    adding link to alpine

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# config
+# config - see https://alpinelinux.org/downloads/ for suitable binaries
 DOWNLOAD_URL=http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/armv7/alpine-minirootfs-3.10.1-armv7.tar.gz
 DEFAULT_DOCKER_TAG=armv7lxu4/alpine-base-armv7
 TAG_VERSION=3.10.1
