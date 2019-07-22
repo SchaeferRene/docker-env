360276d21101054ca664b28d634345571fa25928
R. Schaefer <31whte1tsb5t@opayq.com>
2019-07-22 17:20:00 +0200

    updated readme and fixed create_dockerfile script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# alpine-base-armv7
-creates alpine based armv7 docker base image
+# docker-env-odroid-xu4
+## About
+`docker-env-odroid-xu4` is my personal playground to create Docker images for Odroid-XU4.
+
+## Whats in it?
+* Script to create alpine based armv7 docker base image from scratch
+
--- create_docker_image.sh
+++ create_docker_image.sh
@@ -5,10 +5,14 @@ DOWNLOAD_URL=http://dl-cdn.alpinelinux.org/alpine/v3.10/releases/armv7/alpine-mi
 DEFAULT_DOCKER_TAG=armv7lxu4/alpine-base-armv7
 TAG_VERSION=3.10.1
 
+# misc
+export DOCKER_BUILDKIT=1
+
 # download binaries and create image
 curl -L "$DOWNLOAD_URL" | gunzip | docker import - $DEFAULT_DOCKER_TAG
 docker tag  $DEFAULT_DOCKER_TAG $DEFAULT_DOCKER_TAG:$TAG_VERSION
+docker tag  $DEFAULT_DOCKER_TAG $DEFAULT_DOCKER_TAG:latest
 docker run --rm -it $DEFAULT_DOCKER_TAG echo -e '\n\nSuccess.\n'
 
-docker push $DEFAULT_DOCKER_TAG $DEFAULT_DOCKER_TAG:$TAG_VERSION
-
+docker push $DEFAULT_DOCKER_TAG:$TAG_VERSION
+docker push $DEFAULT_DOCKER_TAG:latest
