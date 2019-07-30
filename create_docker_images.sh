0e2cc353c64d203f69cbb33c520d00eb56877123
R. Schaefer <31whte1tsb5t@opayq.com>
2019-07-31 01:15:54 +0200

    first iteration for setting up nginx proxy

---
+++ create_docker_images.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# remember and change path
+CURRENTPATH=$(pwd)
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPTPATH"
+
+# config
+set -e
+source .env
+export DOCKER_BUILDKIT=1
+BASE_IMAGE=$DOCKER_ID/$BASE_IMAGE_NAME
+
+# check for existing image
+EXISTING_DOCKER_IMAGE=$(docker images -q "${BASE_IMAGE}:${ALPINE_VERSION}" 2> /dev/null)
+if [[ -z "$EXISTING_DOCKER_IMAGE" ]]; then
+	# download binaries and create image
+	curl -L "$ALPINE_DOWNLOAD_URL" | gunzip | docker import - $BASE_IMAGE
+	docker tag  $BASE_IMAGE $BASE_IMAGE:$ALPINE_VERSION
+	docker tag  $BASE_IMAGE $BASE_IMAGE:latest
+	docker run --rm -it $BASE_IMAGE echo -e '\nbuilt successfully\n'
+
+	docker push $BASE_IMAGE:$ALPINE_VERSION
+	docker push $BASE_IMAGE:latest
+fi
+
+# prepare build vars
+#NGINX_MODULES=$(cat nginx-packages.lst | grep "\S" | sed -e "s/$/-${NGINX_VERSION}-r${NGINX_PKG_RELEASE}/" | tr -s '\n' ' ' )
+export NGINX_MODULES=$(cat nginx-packages.lst | grep "\S" | tr -s '\n' ' ' )
+
+# build and deploy services
+docker-compose --verbose up --build -d
+docker-compose logs
+
+cd "$CURRENTPATH"
