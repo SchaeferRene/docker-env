07707fdb5b4a804b81cf7a0dd7e8609df9da244d
Rene Schäfer <z06qpnrxtbf1@opayq.com>
2019-11-27 07:41:09 +0100

    adding command line args
    * adding usage description
    * adding command line arguments
    * updated readme

--- README.md
+++ README.md
@@ -5,7 +5,17 @@
 * armv7h (Odroid-XU4)
 * x86_64 (Schenker XMG)
 
-
 ## Whats in it?
 * Script to create alpine based docker base image from scratch
 * build nginx image on top of base image
+
+# Usage
+The script can be controlled by the following command line arguments
+
+* `-h` | `--help` - display help
+* `-p` | `--push` - push created images to docker registry
+* `-l` | `--logs` - once deployed, follow the logs of deployed services
+* `--nginx` - build nginx service
+
+# Configuration
+*tbd*
--- create_docker_images.sh
+++ create_docker_images.sh
@@ -1,26 +1,74 @@
 #! /bin/bash
-# check permissions
-TEST=$(id | grep '(docker)')
-if [ -z "$TEST" ]
-then
-	echo "$USER must belong to docker group" >&2
-	exit 2
-fi
-
 # remember and change path
 CURRENTPATH=$(pwd)
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPTPATH"
 
-# config
+# usage
+function usage () {
+	echo "usage: $0 [OPTIONS[]"
+	echo
+	echo "This script generates an alpine base image file and other images based on it and deploys them."
+	echo
+	echo "Options:"
+	echo "  -h, --help		Display help and exit"
+	echo "  -p, --push		Push built images to docker registry"
+	echo "  -l, --logs		follow logs of built and deployed services"
+	echo "      --nginx		Build nginx image"
+	exit
+}
+
 # fail on error
 set -e
 
 # read config
 source .env
 
-# prepare base image download URL
+# prepare image names
 export ARCH=$(uname -m)
+export NGINX_IMAGE="$DOCKER_ID/nginx-alpine-$ALPINE_VERSION-$ARCH:latest"
+
+# config
+PUSH_IMAGES=0
+FOLLOW_LOGS=0
+COMPOSE_FILES=""
+PUSH_IMAGE_TAGS=()
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -h|--help)
+        usage
+        ;;
+        -p|--push)
+        PUSH_IMAGES=1
+        ;;
+        -l|--logs)
+        FOLLOW_LOGS=1
+        ;;
+        --nginx)
+	    COMPOSE_FILES="$COMPOSE_FILES -f docker-compose-nginx.yml"
+	    PUSH_IMAGE_TAGS+=("$NGINX_IMAGE")
+        ;;
+        *)
+        echo "Unknown option '$key'" >&2
+        usage
+        ;;
+    esac
+    # Shift after checking all the cases to get the next option
+    shift
+done
+
+# check permissions
+TEST=$(id | grep '(docker)')
+if [ -z "$TEST" ]
+then
+	echo "$USER must belong to docker group" >&2
+	cd "$CURRENTPATH"
+	exit 2
+fi
+
+# prepare base image download URL
 ALPINE_MAJOR_MINOR=$(echo "${ALPINE_VERSION}" | sed -E 's/\.[[:digit:]]+$//')
 ALPINE_DOWNLOAD_URL="http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_MAJOR_MINOR}/releases/${ARCH}/alpine-minirootfs-${ALPINE_VERSION}-${ARCH}.tar.gz"
 
@@ -30,6 +78,7 @@ then
     echo "Using base image from $ALPINE_DOWNLOAD_URL"
 else
     echo "Invalid download URL: $ALPINE_DOWNLOAD_URL" >&2
+	cd "$CURRENTPATH"
     exit 2
 fi
 
@@ -45,8 +94,10 @@ if [[ -z "$EXISTING_DOCKER_IMAGE" ]]; then
 	docker tag  $BASE_IMAGE $BASE_IMAGE:latest
 	docker run --rm -it $BASE_IMAGE echo -e '\nbuilt successfully\n'
 
-	docker push $BASE_IMAGE:$ALPINE_VERSION
-	docker push $BASE_IMAGE:latest
+	if [ $PUSH_IMAGES ]; then
+		docker push $BASE_IMAGE:$ALPINE_VERSION
+		docker push $BASE_IMAGE:latest
+	fi
 else
 	echo "$EXISTING_DOCKER_IMAGE already exists"
 fi
@@ -55,15 +106,24 @@ fi
 export NGINX_MODULES=$(cat nginx-packages.lst | grep "\S" | tr -s '\n' ' ' )
 
 # build and deploy services
-# TODO: add command line parameters
-docker-compose \
-	-f docker-compose-nginx.yml \
-	up --build --force-recreate -d
+if [ -n "$COMPOSE_FILES" ]; then
+	docker-compose $COMPOSE_FILES up --build --force-recreate -d
+	
+	if [ $PUSH_IMAGES ]; then
+		for tag in "${PUSH_IMAGE_TAGS[@]}"
+		do
+	   		docker push $tag
+		done
+	fi
+else
+	echo "no dependent services to compose..."
+fi
 
 # get back to where we started
 cd "$CURRENTPATH"
 
-exit
-
 # follow logs
-docker-compose logs -f
+if [ -n "$COMPOSE_FILES" -a $FOLLOW_LOGS -ne 0 ];
+then
+	docker-compose $COMPOSE_FILES logs -f
+fi
