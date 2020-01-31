949bfa6c5aa80a81868f2be1e0b08cae1ce0b088
Rene Schäfer <31whte1tsb5t@opayq.com>
2020-01-31 09:42:32 +0100

    allowing to build mpd image without setting up pulse socket

--- set_env.sh
+++ set_env.sh
@@ -34,6 +34,10 @@ if [ -e "$PULSE_SOCKET" ]; then
 	export PULSE_UUID=$(stat -c %u "$PULSE_SOCKET" 2>/dev/null)
 	export PULSE_GUID=$(stat -c %g "$PULSE_SOCKET" 2>/dev/null)
 	FEATURES+=(mpd)
+else
+	export PULSE_UUID=$UID
+	export PULSE_GUID=$(id -G | awk '{print $1}')
+	FEATURES+=(mpd)
 fi
 
 # deploy switch
