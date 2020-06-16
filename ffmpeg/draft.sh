c44ca5253833c78307a802dc1c9c1c944d7e6ad7
René Schäfer <31whte1tsb5t@opayq.com>
2020-06-16 20:57:11 +0200

    wip: adding ffmpeg

---
+++ ffmpeg/draft.sh
@@ -0,0 +1,156 @@
+#! /bin/sh
+
+PREFIX=/opt/ffmpeg
+PKG_CONFIG_PATH="$PREFIX/share/pkgconfig:$PREFIX/lib/pkgconfig:$PREFIX/lib64/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/local/lib/pkgconfig:/lib64/pkgconfig:/lib/pkgconfig"
+LD_LIBRARY_PATH="$PREFIX/lib64:$PREFIX/lib:/usr/local/lib64:/usr/local/lib:/usr/lib64:/usr/lib:/lib64:/lib"
+MAKEFLAGS=-j2
+CFLAGS="-O3 -static-libgcc -fno-strict-overflow -fstack-protector-all -fPIE"
+CXXFLAGS="-O3 -static-libgcc -fno-strict-overflow -fstack-protector-all -fPIE"
+PATH="$PREFIX/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+LDFLAGS="-Wl,-z,relro,-z,now"
+
+installDependencies() {
+	apk add --no-cache --update \
+		build-base \
+		autoconf \
+		automake \
+		cmake \
+		pkgconfig \
+		libtool \
+		texinfo \
+		glib-static \
+		git \
+		wget \
+		tar \
+		yasm \
+		zlib-dev \
+		zlib-static \
+		openssl \
+		openssl-dev \
+		openssl-libs-static \
+		zlib-dev \
+		zlib-static \
+		libbz2 \
+		bzip2-dev \
+		bzip2-static \
+		libpng-dev \
+		libpng-static \
+		harfbuzz-dev \
+		harfbuzz-static \
+		brotli-dev \
+		brotli-static \
+		expat-dev \
+		expat-static \
+		fontconfig-dev \
+		freetype-static \
+		fontconfig-static \
+		libxml2-dev \
+		sdl2-dev \
+		sdl2-static \
+		#coreutils \
+		#diffutils \
+		#xz \
+		#nasm \
+		#meson \
+		#ninja \
+		#libxml2-dev \
+		#fribidi-dev \
+		#fribidi-static \
+		#soxr-dev \
+		#soxr-static \
+		#graphite2-static \
+
+}
+
+# compile freetype2
+compileFreetype2() {
+	DIR=/tmp/freetype2
+	mkdir -p "$DIR" && cd "$DIR"
+	git clone --depth 1 https://git.savannah.nongnu.org/git/freetype/freetype2.git
+	cd freetype2/
+	./autogen.sh
+	./configure \
+		--prefix="$PREFIX" \
+		--enable-shared=no \
+		--enable-static=yes \
+		--enable-freetype-config \
+		--with-zlib=yes \
+		--with-bzip2=yes \
+		--with-png=yes \
+		--with-harfbuzz=yes \
+		--with-brotli=yes
+	make && make install
+}
+
+# compile fontconfig
+compileFontConfig() {
+	DIR=/tmp/fontconfig
+	mkdir -p "$DIR" && cd "$DIR"
+	wget -O fontconfig.tar.bz2 https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.1.tar.bz2
+	tar xjf fontconfig.tar.bz2
+	cd fontconfig*
+	./configure \
+		--prefix="$PREFIX" \
+		--enable-static=yes \
+		--enable-shared=no \
+		--disable-docs
+	make && make install
+}
+
+# sanity check
+sanityCheck() {
+	if [[ $? -eq 0 ]]; then
+		echo "${PREFIX}/bin/ffmpeg -version" && ${PREFIX}/bin/ffmpeg -version
+		echo "${PREFIX}/bin/ffprobe -version" && ${PREFIX}/bin/ffmpeg -version
+
+		for PRG in fc-cache fc-cat fc-list fc-match fc-pattern fc-query fc-scan fc-validate freetype-config ffmpeg ffprobe
+		do
+			PRG="$PREFIX/bin/$PRG"
+			if [[ -f "$PRG" ]]; then
+				echo -n "${PRG} dependencies:" && echo $(ldd "$PRG" | wc -l)
+			fi
+		done
+	fi
+}
+
+# compile ffmpeg
+compileFfmpeg() {
+	DIR=/tmp/ffmpeg
+	mkdir -p /tmp/ffmpeg
+	cd /tmp/ffmpeg
+	wget -O ffmpeg-snapshot.tar.bz2 https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+	tar xjf ffmpeg-snapshot.tar.bz2
+	cd ffmpeg/
+	./configure \
+		--pkg-config=pkg-config \
+		--pkg-config-flags=--static \
+		--prefix="$PREFIX" \
+		--extra-cflags="-I${PREFIX}/include -fopenmp" \
+		--extra-ldflags="-static -fopenmp" \
+		--toolchain=hardened \
+		--disable-debug \
+		--disable-shared \
+		--enable-static \
+		--enable-pic \
+		--enable-gpl \
+		--enable-nonfree \
+		--enable-version3 \
+		--disable-doc \
+		--enable-openssl \
+		--enable-libxml2 \
+		#--enable-libfreetype \
+		#--enable-fontconfig \
+
+	make && make install
+}
+
+mkdir -p "$PREFIX"
+
+installDependencies
+#compileFreetype2
+#compileFontConfig
+
+compileFfmpeg
+
+sanityCheck
+
