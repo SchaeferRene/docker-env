1bd7ebb4289b5cbfb5264857e3875ec4ea4f3da4
René Schäfer <31whte1tsb5t@opayq.com>
2020-10-09 23:13:36 +0200

    adding exec flag

--- _set_env/set_env_base.sh
+++ _set_env/set_env_base.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # prepare base image download URL
-ALPINE_MAJOR_MINOR=$(echo "${ALPINE_VERSION}" | sed -E 's/\.[[:digit:]]+$//')
-ALPINE_DOWNLOAD_URL="http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_MAJOR_MINOR}/releases/${ARCH}/alpine-minirootfs-${ALPINE_VERSION}-${ARCH}.tar.gz"
+export ALPINE_MAJOR_MINOR=$(echo "${ALPINE_VERSION}" | sed -E 's/\.[[:digit:]]+$//')
+export ALPINE_DOWNLOAD_URL="http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_MAJOR_MINOR}/releases/${ARCH}/alpine-minirootfs-${ALPINE_VERSION}-${ARCH}.tar.gz"
 
-BASE_IMAGE="alpine-base-$ARCH"
+export BASE_IMAGE="alpine-base-$ARCH"
