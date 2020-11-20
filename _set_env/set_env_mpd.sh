b400b4774febe5c0b32d0660dac24498297851c5
rene-schaefer <f2ugz5n4vwd7@opayq.com>
2020-11-20 12:31:23 +0100

    - updating alpine version

--- _set_env/set_env_mpd.sh
+++ _set_env/set_env_mpd.sh
@@ -6,6 +6,11 @@ if [ -e "$PULSE_SOCKET" ]; then
 	export PULSE_UUID=$(stat -c %u "$PULSE_SOCKET" 2>/dev/null)
 	export PULSE_GUID=$(stat -c %g "$PULSE_SOCKET" 2>/dev/null)
 else
+	# default to 2nd user
+	#export PULSE_UUID=1001
+	#export PULSE_GUID=1001
+	
+	# or disallow build
 	echo "missing pulse socket"
 	exit -1
 fi
