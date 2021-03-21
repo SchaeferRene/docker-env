0257c6bef3a69bda441445f5716fa7edf3e8cbad
René Schäfer <31whte1tsb5t@opayq.com>
2021-03-21 21:10:29 +0100

    fixed naming of youtube-dl image

--- _set_env/set_env_base.sh
+++ _set_env/set_env_base.sh
@@ -5,3 +5,4 @@ export ALPINE_MAJOR_MINOR=$(echo "${ALPINE_VERSION}" | sed -E 's/\.[[:digit:]]+$
 export ALPINE_DOWNLOAD_URL="http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_MAJOR_MINOR}/releases/${ARCH}/alpine-minirootfs-${ALPINE_VERSION}-${ARCH}.tar.gz"
 
 export BASE_IMAGE="alpine-base-$ARCH"
+export YDL_IMAGE="youtube-dl-alpine-$ARCH"
